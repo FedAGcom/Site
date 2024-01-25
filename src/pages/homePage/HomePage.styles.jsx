f3d75ba3357eeb24211321184f9935a30a9e6f8d
Ekaterina <frna338@gmail.com>
2024-01-25 13:35:13 +0400

    Fixing max-width at mobile

--- src/pages/homePage/HomePage.styles.jsx
+++ src/pages/homePage/HomePage.styles.jsx
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const StyledHomePage = styled.div`
 	// min-height: 6890px;
-	// @media only screen and (max-width: 600px) {
-    // min-height: ${({ russian }) => russian === "true" ? "9110px" : "9033px"};
-	// }
+	@media only screen and (max-width: 600px) {
+    //min-height: ${({ russian }) => russian === "true" ? "9110px" : "9033px"};
+	overflow-x: hidden;
+	}
 `;
